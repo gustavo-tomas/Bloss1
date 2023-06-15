fbbde3bc996d3ece4a4058521297240fe78f4c74
gustavo-tomas <guga.tomas10@gmail.com>
2023-06-15 15:20:59 -0300

    Removing prints

--- bloss1/src/ecs/systems.hpp
+++ bloss1/src/ecs/systems.hpp
@@ -10,27 +10,19 @@ namespace bls
 {
     void render_system(ECS& ecs, const f32& dt)
     {
-        std::cout << "\nRenderSystem:\n";
-
         auto& models = ecs.models;
         for (auto& [id, model] : models)
         {
             model->mesh += 200 * dt;
-
-            std::cout << " - mesh: " << model->mesh << "\n";
         }
     }
 
     void transform_system(ECS& ecs, const f32& dt)
     {
-        std::cout << "\nTransformSystem:\n";
-
         auto& transforms = ecs.transforms;
         for (auto& [id, transform] : transforms)
         {
             transform->scale += 400 * dt;
-
-            std::cout << " - scale: " << transform->scale << "\n";
         }
     }
 };
