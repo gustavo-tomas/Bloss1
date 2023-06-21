08764b7c84563376f2db2dfc0b1ca4eff5b739be
gustavo-tomas <guga.tomas10@gmail.com>
2023-06-21 09:42:13 -0300

    Adding typedefs for glm types

---
+++ bloss1/src/math/mat.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+/**
+ * @brief @TODO
+ */
+
+namespace bls
+{
+
+};
--- bloss1/src/math/math.hpp
+++ bloss1/src/math/math.hpp
@@ -6,40 +6,18 @@
 
 // @TODO: type checking, unions, simd or smth
 
-#include "core/core.hpp"
+#include "math/mat.hpp"
+#include "math/vec.hpp"
+#include "math/to_str.hpp"
 
 #include <glm/glm.hpp>
 
 namespace bls
 {
-    template<typename T>
-    class Vec4
-    {
-        public:
-            Vec4(T x, T y, T z, T w)
-            {
-                this->x = x;
-                this->y = y;
-                this->z = z;
-                this->w = w;
-            }
+    typedef glm::vec4 vec4;
+    typedef glm::vec3 vec3;
+    typedef glm::vec2 vec2;
 
-            union { T x, r, s; };
-            union { T y, g, t; };
-            union { T z, b, p; };
-            union { T w, a, q; };
-    };
-
-    typedef Vec4<f32> vec4;
-
-    // Helper function for string conversion
-    inline str to_str(const vec4& vec)
-    {
-        const str x = std::to_string(vec.x);
-        const str y = std::to_string(vec.y);
-        const str z = std::to_string(vec.z);
-        const str w = std::to_string(vec.w);
-
-        return "(" + x + ", " + y + ", " + z + ", " + w + ")";
-    }
+    typedef glm::mat4 mat4;
+    typedef glm::mat3 mat3;
 };
