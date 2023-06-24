b8a848240c550d96114cce94209d50a4ca0680fc
gustavo-tomas <guga.tomas10@gmail.com>
2023-06-24 13:31:47 -0300

    Indexing vertices of cube primitive

--- bloss1/src/renderer/primitives/cube.hpp
+++ bloss1/src/renderer/primitives/cube.hpp
@@ -13,72 +13,29 @@ namespace bls
         public:
             Cube(Renderer& renderer) : renderer(renderer)
             {
-                f32 cubeVertices[] =
-                {
-                    // positions
-                    (-1.0f), 1.0f, -1.0f,
-                    -1.0f, -1.0f, -1.0f,
-                    1.0f, -1.0f, -1.0f,
-                    1.0f, -1.0f, -1.0f,
-                    1.0f,  1.0f, -1.0f,
-                    -1.0f,  1.0f, -1.0f,
-
-                    -1.0f, -1.0f,  1.0f,
-                    -1.0f, -1.0f, -1.0f,
-                    -1.0f,  1.0f, -1.0f,
-                    -1.0f,  1.0f, -1.0f,
-                    -1.0f,  1.0f,  1.0f,
-                    -1.0f, -1.0f,  1.0f,
-
-                    1.0f, -1.0f, -1.0f,
-                    1.0f, -1.0f,  1.0f,
-                    1.0f,  1.0f,  1.0f,
-                    1.0f,  1.0f,  1.0f,
-                    1.0f,  1.0f, -1.0f,
-                    1.0f, -1.0f, -1.0f,
-
-                    -1.0f, -1.0f,  1.0f,
-                    -1.0f,  1.0f,  1.0f,
-                    1.0f,  1.0f,  1.0f,
-                    1.0f,  1.0f,  1.0f,
-                    1.0f, -1.0f,  1.0f,
-                    -1.0f, -1.0f,  1.0f,
-
-                    -1.0f,  1.0f, -1.0f,
-                    1.0f,  1.0f, -1.0f,
-                    1.0f,  1.0f,  1.0f,
-                    1.0f,  1.0f,  1.0f,
-                    -1.0f,  1.0f,  1.0f,
-                    -1.0f,  1.0f, -1.0f,
-
-                    -1.0f, -1.0f, -1.0f,
-                    -1.0f, -1.0f,  1.0f,
-                    1.0f, -1.0f, -1.0f,
-                    1.0f, -1.0f, -1.0f,
-                    -1.0f, -1.0f, 1.0f,
-                    1.0f, -1.0f,  1.0f
-                };
-
                 // Setup cube VAO
                 vao = VertexArray::create();
                 vao->bind();
 
-                vbo = VertexBuffer::create(cubeVertices, sizeof(cubeVertices));
-                vao->add_vertex_buffer(0, 3, ShaderDataType::Float, false, 3 * sizeof(float), (void*) 0);
+                vbo = VertexBuffer::create(vertices, sizeof(vertices));
+                ebo = IndexBuffer::create(indices, sizeof(indices) / sizeof(u32));
+
+                vao->add_vertex_buffer(0, 3, ShaderDataType::Float, false, 0, (void*) 0);
             };
 
             ~Cube()
             {
                 delete vao;
                 delete vbo;
+                delete ebo;
 
-                std::cout << "Cube destroyed successfully\n";
+                std::cout << "cube destroyed successfully\n";
             };
 
             void Render()
             {
                 vao->bind();
-                renderer.draw_arrays(36);
+                renderer.draw_indexed(sizeof(indices) / sizeof(u32));
                 vao->unbind();
             };
 
@@ -86,5 +43,48 @@ namespace bls
             Renderer& renderer;
             VertexArray* vao;
             VertexBuffer* vbo;
+            IndexBuffer* ebo;
+
+            f32 vertices[24] =
+            {
+                // Front face
+                -1.0f, -1.0f, 1.0f, // (0)
+                1.0f,  -1.0f, 1.0f, // (1)
+                1.0f,   1.0f, 1.0f, // (2)
+                -1.0f,  1.0f, 1.0f, // (3)
+
+                // Back face
+                -1.0f, -1.0f, -1.0f, // (4)
+                1.0f,  -1.0f, -1.0f, // (5)
+                1.0f,   1.0f, -1.0f, // (6)
+                -1.0f,  1.0f, -1.0f  // (7)
+            };
+
+            u32 indices[36] =
+            {
+                // Front face
+                0, 1, 2,
+                2, 3, 0,
+
+                // Right face
+                1, 5, 6,
+                6, 2, 1,
+
+                // Back face
+                7, 6, 5,
+                5, 4, 7,
+
+                // Left face
+                4, 0, 3,
+                3, 7, 4,
+
+                // Bottom face
+                4, 5, 1,
+                1, 0, 4,
+
+                // Top face
+                3, 2, 6,
+                6, 7, 3
+            };
     };
 };
