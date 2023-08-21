0839f0c5ef066899e3b64f4a294dfba9011c2402
gustavo-tomas <guga.tomas10@gmail.com>
2023-08-21 10:26:05 -0300

    Simplifying collision test and resolve

--- bloss1/src/ecs/systems/physics_system.cpp
+++ bloss1/src/ecs/systems/physics_system.cpp
@@ -4,10 +4,9 @@
 // @TODO: finish and cleanup
 namespace bls
 {
-    // @TODO: finish
     void physics_system(ECS& ecs, f32 dt)
     {
-        resolve_collisions(ecs, dt);
+        resolve_collisions(ecs);
 
         auto& transforms = ecs.transforms;
         auto& objects = ecs.physics_objects;
@@ -43,19 +42,14 @@ namespace bls
                 object->velocity.y = 0.0f;
             }
 
-            std::cout << "POS: " << transforms[id]->position.x << ", " << transforms[id]->position.y << ", " << transforms[id]->position.z << "\n";
-            std::cout << "VEL: " << object->velocity.x << ", " << object->velocity.y << ", " << object->velocity.z << "\n";
-
             // Reset forces
             object->force = vec3(0.0f);
         }
     }
 
-    void resolve_collisions(ECS& ecs, f32 dt)
+    void resolve_collisions(ECS& ecs)
     {
-        auto& objects = ecs.physics_objects;
         auto& colliders = ecs.colliders;
-        auto& transforms = ecs.transforms;
         for (auto& [id_a, collider_a] : colliders)
         {
             // Assume no collision happens
@@ -67,207 +61,208 @@ namespace bls
                 if (id_a == id_b)
                     break;
 
-                auto transform_a = transforms[id_a].get();
-                auto transform_b = transforms[id_b].get();
-                auto collision = test_collision(collider_a.get(), transform_a, collider_b.get(), transform_b);
-
+                // Solve collision
+                auto collision = test_collision(ecs, id_a, id_b);
                 if (collision.has_collision)
                 {
                     collider_a->color = { 1.0f, 0.0f, 0.0f };
                     collider_b->color = { 1.0f, 0.0f, 0.0f };
-                    solve_collision(transform_a, objects[id_a].get(), collider_a.get(),
-                                    transform_b, objects[id_b].get(), collider_b.get(),
-                                    collision, dt);
+                    solve_collision(ecs, id_a, id_b, collision);
                 }
             }
         }
     }
 
-    Collision test_collision(Collider* col_a, Transform* trans_a, Collider* col_b, Transform* trans_b)
+    // Collision tester
+    // -----------------------------------------------------------------------------------------------------------------
+    Collision test_collision(ECS& ecs, u32 id_a, u32 id_b)
     {
-        // Box v. Sphere
-        if (col_a->type == Collider::Box && col_b->type == Collider::Sphere)
-            return test_collision(static_cast<BoxCollider*>(col_a), trans_a, static_cast<SphereCollider*>(col_b), trans_b);
+        auto collider_a = ecs.colliders[id_a].get();
+        auto collider_b = ecs.colliders[id_b].get();
+
+        auto trans_a = ecs.transforms[id_a].get();
+        auto trans_b = ecs.transforms[id_b].get();
+
+        Collision collision = { };
 
         // Sphere v. Box
-        else if (col_a->type == Collider::Sphere && col_b->type == Collider::Box)
-            return test_collision(static_cast<BoxCollider*>(col_b), trans_b, static_cast<SphereCollider*>(col_a), trans_a);
+        if (collider_a->type == Collider::Sphere && collider_b->type == Collider::Box)
+        {
+            // Swap
+            auto temp = collider_a;
+            collider_a = collider_b;
+            collider_b = temp;
+        }
+
+        // Box v. Sphere
+        if (collider_a->type == Collider::Box && collider_b->type == Collider::Sphere)
+        {
+            auto col_a = static_cast<BoxCollider*>(collider_a);
+            auto col_b = static_cast<SphereCollider*>(collider_b);
+
+            // Point where the box 'begins'
+            f32 min_x = trans_a->position.x - col_a->width;
+            f32 min_y = trans_a->position.y - col_a->height;
+            f32 min_z = trans_a->position.z - col_a->depth;
+
+            // Point where the box 'ends'
+            f32 max_x = trans_a->position.x + col_a->width;
+            f32 max_y = trans_a->position.y + col_a->height;
+            f32 max_z = trans_a->position.z + col_a->depth;
+
+            f32 x = max(min_x, min(trans_b->position.x, max_x));
+            f32 y = max(min_y, min(trans_b->position.y, max_y));
+            f32 z = max(min_z, min(trans_b->position.z, max_z));
+
+            // Distance squared
+            f32 distance_2 = (
+                                 (x - trans_b->position.x) * (x - trans_b->position.x) +
+                                 (y - trans_b->position.y) * (y - trans_b->position.y) +
+                                 (z - trans_b->position.z) * (z - trans_b->position.z)
+                             );
+
+            // @TODO
+            if (distance_2 < col_b->radius * col_b->radius)
+            {
+                // Calculate the penetration depth along each axis
+                f32 penetrationX = 0.0, penetrationY = 0.0, penetrationZ = 0.0;
+
+                if (trans_b->position.x < min_x)
+                    penetrationX = min_x - trans_b->position.x;
+                else if (trans_b->position.x > max_x)
+                    penetrationX = trans_b->position.x - max_x;
+
+                if (trans_b->position.y < min_y)
+                    penetrationY = min_y - trans_b->position.y;
+                else if (trans_b->position.y > max_y)
+                    penetrationY = col_b->radius - (trans_b->position.y - max_y);
+
+                if (trans_b->position.z < min_z)
+                    penetrationZ = min_z - trans_b->position.z;
+                else if (trans_b->position.z > max_z)
+                    penetrationZ = trans_b->position.z - max_z;
+
+                collision.point_a = vec3(penetrationX, penetrationY, penetrationZ);
+                collision.point_b = vec3(0.0f);
+                collision.has_collision = true;
+            }
+
+            return collision;
+        }
 
         // Sphere v. Sphere
-        else if (col_a->type == Collider::Sphere && col_b->type == Collider::Sphere)
-            return test_collision(static_cast<SphereCollider*>(col_a), trans_a, static_cast<SphereCollider*>(col_b), trans_b);
+        else if (collider_a->type == Collider::Sphere && collider_b->type == Collider::Sphere)
+        {
+            auto col_a = static_cast<SphereCollider*>(collider_a);
+            auto col_b = static_cast<SphereCollider*>(collider_b);
+
+            auto dist = distance(trans_a->position, trans_b->position);
+            if (dist < col_a->radius + col_b->radius)
+            {
+                vec3 pa = trans_a->position + (dist - col_b->radius);
+                vec3 pb = trans_b->position + (dist - col_a->radius);
+
+                collision.point_a = pa;
+                collision.point_b = pb;
+                collision.has_collision = true;
+            }
+
+            return collision;
+        }
 
         // Box v. Box
-        else if (col_a->type == Collider::Box && col_b->type == Collider::Box)
-            return test_collision(static_cast<BoxCollider*>(col_a), trans_a, static_cast<BoxCollider*>(col_b), trans_b);
+        else if (collider_a->type == Collider::Box && collider_b->type == Collider::Box)
+        {
+            auto col_a = static_cast<BoxCollider*>(collider_a);
+            auto col_b = static_cast<BoxCollider*>(collider_b);
+
+            // Point where the box 'begins'
+            f32 min_x_a = trans_a->position.x - col_a->width;
+            f32 min_y_a = trans_a->position.y - col_a->height;
+            f32 min_z_a = trans_a->position.z - col_a->depth;
+
+            f32 min_x_b = trans_b->position.x - col_b->width;
+            f32 min_y_b = trans_b->position.y - col_b->height;
+            f32 min_z_b = trans_b->position.z - col_b->depth;
+
+            // Point where the box 'ends'
+            f32 max_x_a = trans_a->position.x + col_a->width;
+            f32 max_y_a = trans_a->position.y + col_a->height;
+            f32 max_z_a = trans_a->position.z + col_a->depth;
+
+            f32 max_x_b = trans_b->position.x + col_b->width;
+            f32 max_y_b = trans_b->position.y + col_b->height;
+            f32 max_z_b = trans_b->position.z + col_b->depth;
+
+            return { };
+
+            // return (
+            //            min_x_a <= max_x_b &&
+            //            max_x_a >= min_x_b &&
+            //            min_y_a <= max_y_b &&
+            //            max_y_a >= min_y_b &&
+            //            min_z_a <= max_z_b &&
+            //            max_z_a >= min_z_b
+            //        );
+        }
 
         // Invalid colliders
         std::cerr << "invalid colliders\n";
         exit(1);
     }
 
-    void solve_collision(Transform* trans_a, PhysicsObject* object_a, Collider* collider_a,
-                         Transform* trans_b, PhysicsObject* object_b, Collider* collider_b,
-                         Collision collision, f32 dt)
+    // Collision solver
+    // -----------------------------------------------------------------------------------------------------------------
+    void solve_collision(ECS& ecs, u32 id_a, u32 id_b, Collision collision)
     {
-        // Box v. Sphere
-        if (collider_a->type == Collider::Box && collider_b->type == Collider::Sphere)
-            return solve_collision(trans_a, object_a, static_cast<BoxCollider*>(collider_a),
-                                   trans_b, object_b, static_cast<SphereCollider*>(collider_b),
-                                   collision, dt);
+        auto collider_a = ecs.colliders[id_a].get();
+        auto collider_b = ecs.colliders[id_b].get();
+
+        auto trans_a = ecs.transforms[id_a].get();
+        auto trans_b = ecs.transforms[id_b].get();
 
         // Sphere v. Box
-        else if (collider_a->type == Collider::Sphere && collider_b->type == Collider::Box)
-            return solve_collision(trans_b, object_b, static_cast<BoxCollider*>(collider_b),
-                                   trans_a, object_a, static_cast<SphereCollider*>(collider_a),
-                                   collision, dt);
+        if (collider_a->type == Collider::Sphere && collider_b->type == Collider::Box)
+        {
+            // Swap
+            auto temp = collider_a;
+            collider_a = collider_b;
+            collider_b = temp;
+        }
+
+        // Box v. Sphere
+        if (collider_a->type == Collider::Box && collider_b->type == Collider::Sphere)
+        {
+            // Move the sphere to resolve overlap
+            trans_b->position += vec3(0.0f, collision.point_a.y, 0.0f);
+            return;
+        }
 
         // Sphere v. Sphere
         else if (collider_a->type == Collider::Sphere && collider_b->type == Collider::Sphere)
-            return solve_collision(trans_a, object_a, static_cast<SphereCollider*>(collider_a),
-                                   trans_b, object_b, static_cast<SphereCollider*>(collider_b),
-                                   collision, dt);
+        {
+            auto col_a = static_cast<SphereCollider*>(collider_a);
+            auto col_b = static_cast<SphereCollider*>(collider_b);
+
+            vec3 normal = collision.point_a - collision.point_b;
+            f32 dist = glm::length(normal);
+
+            normal /= dist;
+
+            // Calculate the separation distance (overlap)
+            f32 overlap = col_a->radius + col_b->radius - dist;
+
+            // Move the spheres to resolve overlap
+            trans_a->position += normal * overlap * 0.5f;
+            trans_b->position -= normal * overlap * 0.5f;
+            return;
+        }
 
         // Box v. Box
         // else if (collider_a->type == Collider::Box && collider_b->type == Collider::Box)
         //     return solve_collision(trans_a, object_a, static_cast<BoxCollider*>(collider_a),
         //                            trans_b, static_cast<BoxCollider*>(collider_b),
         //                            collision, dt);
-    }
-
-    // Sphere v. Sphere
-    void solve_collision(Transform* trans_a, PhysicsObject* object_a, SphereCollider* collider_a,
-                         Transform* trans_b, PhysicsObject* object_b, SphereCollider* collider_b,
-                         Collision collision, f32 dt)
-    {
-        vec3 normal = collision.point_a - collision.point_b;
-        // vec3 normal = trans_a->position - trans_b->position;
-        f32 dist = glm::length(normal);
-
-        normal /= dist;
-
-        // Calculate the separation distance (overlap)
-        f32 overlap = collider_a->radius + collider_b->radius - dist;
-
-        // Move the spheres to resolve overlap
-        trans_a->position += normal * overlap * 0.5f;
-        trans_b->position -= normal * overlap * 0.5f;
-    }
-
-    // Box v. Sphere
-    void solve_collision(Transform* trans_a, PhysicsObject* object_a, BoxCollider* collider_a,
-                         Transform* trans_b, PhysicsObject* object_b, SphereCollider* collider_b,
-                         Collision collision, f32 dt)
-    {
-        // Move the sphere to resolve overlap
-        trans_b->position += vec3(0.0f, collision.point_a.y, 0.0f);
-    }
-
-    // Sphere v. Sphere
-    Collision test_collision(SphereCollider* col_a, Transform* trans_a, SphereCollider* col_b, Transform* trans_b)
-    {
-        Collision collision = { };
-
-        auto dist = distance(trans_a->position, trans_b->position);
-        if (dist < col_a->radius + col_b->radius)
-        {
-            vec3 pa = trans_a->position + (dist - col_b->radius);
-            vec3 pb = trans_b->position + (dist - col_a->radius);
-
-            collision.point_a = pa;
-            collision.point_b = pb;
-            collision.has_collision = true;
-        }
-
-        return collision;
-    }
-
-    // Box v. Sphere
-    Collision test_collision(BoxCollider* col_a, Transform* trans_a, SphereCollider* col_b, Transform* trans_b)
-    {
-        Collision collision = { };
-
-        // Point where the box 'begins'
-        f32 min_x = trans_a->position.x - col_a->width;
-        f32 min_y = trans_a->position.y - col_a->height;
-        f32 min_z = trans_a->position.z - col_a->depth;
-
-        // Point where the box 'ends'
-        f32 max_x = trans_a->position.x + col_a->width;
-        f32 max_y = trans_a->position.y + col_a->height;
-        f32 max_z = trans_a->position.z + col_a->depth;
-
-        f32 x = max(min_x, min(trans_b->position.x, max_x));
-        f32 y = max(min_y, min(trans_b->position.y, max_y));
-        f32 z = max(min_z, min(trans_b->position.z, max_z));
-
-        // Distance squared
-        f32 distance_2 = (
-                             (x - trans_b->position.x) * (x - trans_b->position.x) +
-                             (y - trans_b->position.y) * (y - trans_b->position.y) +
-                             (z - trans_b->position.z) * (z - trans_b->position.z)
-                         );
-
-        // @TODO
-        if (distance_2 < col_b->radius * col_b->radius)
-        {
-            // Calculate the penetration depth along each axis
-            f32 penetrationX = 0.0, penetrationY = 0.0, penetrationZ = 0.0;
-
-            if (trans_b->position.x < min_x)
-                penetrationX = min_x - trans_b->position.x;
-            else if (trans_b->position.x > max_x)
-                penetrationX = trans_b->position.x - max_x;
-
-            if (trans_b->position.y < min_y)
-                penetrationY = min_y - trans_b->position.y;
-            else if (trans_b->position.y > max_y)
-                penetrationY = col_b->radius - (trans_b->position.y - max_y);
-
-            if (trans_b->position.z < min_z)
-                penetrationZ = min_z - trans_b->position.z;
-            else if (trans_b->position.z > max_z)
-                penetrationZ = trans_b->position.z - max_z;
-
-            collision.point_a = vec3(penetrationX, penetrationY, penetrationZ);
-            collision.point_b = vec3(0.0f);
-            collision.has_collision = true;
-        }
-
-        return collision;
-    }
-
-    // Box v. Box
-    Collision test_collision(BoxCollider* col_a, Transform* trans_a, BoxCollider* col_b, Transform* trans_b)
-    {
-        // Point where the box 'begins'
-        f32 min_x_a = trans_a->position.x - col_a->width;
-        f32 min_y_a = trans_a->position.y - col_a->height;
-        f32 min_z_a = trans_a->position.z - col_a->depth;
-
-        f32 min_x_b = trans_b->position.x - col_b->width;
-        f32 min_y_b = trans_b->position.y - col_b->height;
-        f32 min_z_b = trans_b->position.z - col_b->depth;
-
-        // Point where the box 'ends'
-        f32 max_x_a = trans_a->position.x + col_a->width;
-        f32 max_y_a = trans_a->position.y + col_a->height;
-        f32 max_z_a = trans_a->position.z + col_a->depth;
-
-        f32 max_x_b = trans_b->position.x + col_b->width;
-        f32 max_y_b = trans_b->position.y + col_b->height;
-        f32 max_z_b = trans_b->position.z + col_b->depth;
-
-        return { };
-
-        // return (
-        //            min_x_a <= max_x_b &&
-        //            max_x_a >= min_x_b &&
-        //            min_y_a <= max_y_b &&
-        //            max_y_a >= min_y_b &&
-        //            min_z_a <= max_z_b &&
-        //            max_z_a >= min_z_b
-        //        );
     }
 
     f32 apply_deceleration(f32 velocity, f32 deceleration, f32 mass, f32 dt)
--- bloss1/src/ecs/systems/physics_system.hpp
+++ bloss1/src/ecs/systems/physics_system.hpp
@@ -16,28 +16,9 @@ namespace bls
         bool has_collision;
     };
 
-    void resolve_collisions(ECS& ecs, f32 dt);
-
-    Collision test_collision(Collider* col_a, Transform* trans_a, Collider* col_b, Transform* trans_b);
-    Collision test_collision(BoxCollider* col_a, Transform* trans_a, SphereCollider* col_b, Transform* trans_b);
-    Collision test_collision(SphereCollider* col_a, Transform* trans_a, SphereCollider* col_b, Transform* trans_b);
-    Collision test_collision(BoxCollider* col_a, Transform* trans_a, BoxCollider* col_b, Transform* trans_b);
-
-    void solve_collision(Transform* trans_a, PhysicsObject* object_a, Collider* collider_a,
-                         Transform* trans_b, PhysicsObject* object_b, Collider* collider_b,
-                         Collision collision, f32 dt);
-
-    void solve_collision(Transform* trans_a, PhysicsObject* object_a, SphereCollider* collider_a,
-                         Transform* trans_b, PhysicsObject* object_b, SphereCollider* collider_b,
-                         Collision collision, f32 dt);
-
-    void solve_collision(Transform* trans_a, PhysicsObject* object_a, BoxCollider* collider_a,
-                         Transform* trans_b, PhysicsObject* object_b, SphereCollider* collider_b,
-                         Collision collision, f32 dt);
-
-    void solve_collision(Transform* trans_a, PhysicsObject* object_a, Collider* collider_a,
-                         Transform* trans_b, PhysicsObject* object_b, Collider* collider_b,
-                         Collision collision, f32 dt);
+    void resolve_collisions(ECS& ecs);
+    Collision test_collision(ECS& ecs, u32 id_a, u32 id_b);
+    void solve_collision(ECS& ecs, u32 id_a, u32 id_b, Collision collision);
 
     f32 apply_deceleration(f32 velocity, f32 deceleration, f32 mass, f32 dt);
 };
