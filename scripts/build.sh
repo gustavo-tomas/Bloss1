ba3f132729b6de8b9ddc9deb2fd5bd2edd4b2c26
gustavo-tomas <guga.tomas10@gmail.com>
2023-09-01 09:58:51 -0300

    Adding j4 flag for multiple core usage

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,9 +3,9 @@
 # Generate makefiles and binaries in the specified configuration
 
 if [[ -z $1 ]]; then
-    vendor/premake/premake5 gmake && make
+    vendor/premake/premake5 gmake && make -j4
 else
-    vendor/premake/premake5 gmake && make config=$1
+    vendor/premake/premake5 gmake && make config=$1 -j4
 fi
 
 exit 0
