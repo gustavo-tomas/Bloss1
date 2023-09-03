35998bf62c47605a5d3ed2f4fea782f70e14714f
gustavo-tomas <guga.tomas10@gmail.com>
2023-09-03 14:57:01 -0300

    Adding windows premake

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,24 +5,32 @@
 
 echo "OS type: $OSTYPE"
 
-run_gmake () {
+run_gmake_linux () {
     if [[ -z $1 ]]; then
-        vendor/premake/premake5 gmake2 && make -j4
+        vendor/premake/premake5_linux gmake2 && make -j4
     else
-        vendor/premake/premake5 gmake2 && make config=$1 -j4
+        vendor/premake/premake5_linux gmake2 && make config=$1 -j4
+    fi
+}
+
+run_gmake_windows () {
+    if [[ -z $1 ]]; then
+        vendor/premake/premake5_windows.exe gmake2 && make -j4
+    else
+        vendor/premake/premake5_windows.exe gmake2 && make config=$1 -j4
     fi
 }
 
 # Linux
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    run_gmake $1
+    run_gmake_linux $1
 
 # Windows
 elif [[ "$OSTYPE" == "cygwin" ]]; then
     echo "@TODO: POSIX compatibility layer and Linux environment emulation for Windows"
 
 elif [[ "$OSTYPE" == "msys" ]]; then
-    run_gmake $1
+    run_gmake_windows $1
 
 # MAC
 else
