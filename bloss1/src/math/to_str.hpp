d59a245e77fab392f40aba1f7a83a3782b4c98a0
gustavo-tomas <guga.tomas10@gmail.com>
2023-10-25 12:54:36 -0300

    Adding vec2 to_str

--- bloss1/src/math/to_str.hpp
+++ bloss1/src/math/to_str.hpp
@@ -18,6 +18,13 @@ namespace bls
         return vec_str;
     }
 
+    inline str to_str(const glm::vec2& vec)
+    {
+        const str vec_str = glm::to_string(vec);
+
+        return vec_str;
+    }
+
     template<typename T>
     inline str to_str(T num)
     {
